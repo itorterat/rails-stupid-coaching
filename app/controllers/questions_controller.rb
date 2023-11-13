89daa6c8218544b7bba3833089297f086841741e
Irvin TORTERAT <irvin.torterat@gmail.com>
2023-11-13 14:35:30 +0100

    Add answer_enhanced if someone shouts and its tests
    Signed-off-by: Irvin TORTERAT <irvin.torterat@gmail.com>

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -2,11 +2,21 @@ class QuestionsController < ApplicationController
   def ask; end
 
   def answer
-    return unless params[:question]
+    @answer = if params[:question].strip.empty?
+                'Speak to me!'
+              elsif params[:question] == params[:question].upcase
+                "I can feel your motivation! #{answer_enhanced}"
+              else
+                answer_enhanced
+              end
+  end
 
+  private
+
+  def answer_enhanced
     @answer = if params[:question].include?('?')
                 'Silly question, get dressed and go to work!'
-              elsif params[:question] == 'I am going to work right now!'
+              elsif params[:question].upcase == 'I AM GOING TO WORK RIGHT NOW!'
                 'Great!'
               else
                 "I don't care, get dressed and go to work!"
--- test/system/questions_test.rb
+++ test/system/questions_test.rb
@@ -14,6 +14,14 @@ class QuestionsTest < ApplicationSystemTestCase
     assert_text "I don't care, get dressed and go to work!"
   end
 
+  test 'saying nothing yields a response from the coach who wants something' do
+    visit ask_url
+    fill_in 'question', with: '   '
+    click_on 'Ask'
+
+    assert_text 'Speak to me!'
+  end
+
   test 'saying "I am going to work right now!" yields a good response from the coach' do
     visit ask_url
     fill_in 'question', with: 'I am going to work right now!'
@@ -28,5 +36,29 @@ class QuestionsTest < ApplicationSystemTestCase
     click_on 'Ask'
 
     assert_text 'Silly question, get dressed and go to work!'
+  end
+
+  test 'shouting HELLO yields a middle-grumpy response from the coach' do
+    visit ask_url
+    fill_in 'question', with: 'HELLO'
+    click_on 'Ask'
+
+    assert_text "I can feel your motivation! I don't care, get dressed and go to work!"
+  end
+
+  test 'shouting "I AM GOING TO WORK RIGHT NOW!" yields a very good response from the coach' do
+    visit ask_url
+    fill_in 'question', with: 'I AM GOING TO WORK RIGHT NOW!'
+    click_on 'Ask'
+
+    assert_text 'I can feel your motivation! Great!'
+  end
+
+  test 'asking a question while shouting yields a middle-mean response from the coach' do
+    visit ask_url
+    fill_in 'question', with: 'HELLO?'
+    click_on 'Ask'
+
+    assert_text 'I can feel your motivation! Silly question, get dressed and go to work!'
   end
 end
